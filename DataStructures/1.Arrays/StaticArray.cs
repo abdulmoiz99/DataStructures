1e9ceef4845627447e44ae8370de898a48eb9e83
Abdul Moiz <moiza0799@gmail.com>
2024-06-10 16:53:52 -0500

    Add project files.

---
+++ DataStructures/1.Arrays/StaticArray.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DataStructures._1.Arrays
+{
+    internal class StaticArray
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Want to run this file");
+        }
+    }
+}
